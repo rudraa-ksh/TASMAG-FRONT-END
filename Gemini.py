03a9e014376f44bac9e293c25fd213782ec83825
rudraa-ksh <rudrak04sh@gmail.com>
2025-04-10 22:58:11 +0530

    Add: Image Input in Gemini

--- Gemini.py
+++ Gemini.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 import PIL.Image
 
-image = PIL.Image.open('Image.jpg')
+image = PIL.Image.open('Images/Image 2.png')
 
 client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents="What is my name?")
+    contents=["This is school/college time table of a student, please share the detailed information of each row and column in the image in nicely json format in human readable format. I only need day wise schedule with timing and subject.",image])
 
 print(response.text)
