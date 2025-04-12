eabd586e9367e8131bdb278df17af1af0ad83ef7
rudraa-ksh <rudrak04sh@gmail.com>
2025-04-12 20:58:01 +0530

    Optimize: Promt Token Size

--- Gemini.py
+++ Gemini.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 import PIL.Image
 
-image = PIL.Image.open('Images/Image 2.png')
+image = PIL.Image.open('Images/Image3.png')
 
 client = genai.Client(api_key=os.getenv("GEMINI_KEY"))
 response = client.models.generate_content(
     model="gemini-2.0-flash",
-    contents=["This is school/college time table of a student, please share the detailed information of each row and column in the image in nicely json format in human readable format. I only need day wise schedule with timing and subject.",image])
+    contents=["Give day-wise unique subjects from this timetable in JSON.",image])
 
 print(response.text)
